2180939b2b9a24777864b19be1b794d7c6d42c9b
Prince Chidera <princechidera1007@gmail.com>
2025-07-15 15:49:37 +0200

    removed dashboard from universal nav

--- src/app/components/NavbarWrapper.js
+++ src/app/components/NavbarWrapper.js
@@ -7,7 +7,7 @@ import ResponsiveNavbar from './navbar';
 
 export default function NavbarWrapper() {
   const pathname = usePathname();
-  const hideNavbarRoutes = ['/upload']; // Add more paths if needed
+  const hideNavbarRoutes = ['/upload','/dashboard']; // Add more paths if needed
 
   if (hideNavbarRoutes.includes(pathname)) {
     return null;
