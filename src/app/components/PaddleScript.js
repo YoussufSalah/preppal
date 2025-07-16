ea8ba6c6554c910d755146f6c4db03156a27c426
YoussufSalah <youssufsalahsaber@gmail.com>
2025-07-16 14:30:33 +0300

    Updated PaddleScript

--- src/app/components/PaddleScript.js
+++ src/app/components/PaddleScript.js
@@ -8,13 +8,19 @@ export default function PaddleScript() {
             src="https://cdn.paddle.com/paddle/v2/paddle.js"
             strategy="beforeInteractive"
             onLoad={() => {
-                if (typeof window !== "undefined" && window.Paddle) {
+                if (window.Paddle) {
                     window.Paddle.Environment.set("sandbox");
                     window.Paddle.Initialize({
                         token: process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN,
-                        eventCallback: function (data) {
-                            console.log(data);
+                        checkout: {
+                            settings: {
+                                displayMode: "overlay",
+                                theme: "light",
+                                locale: "en",
+                            },
                         },
+                        eventCallback: (data) =>
+                            console.log("Paddle event:", data),
                     });
                 }
             }}
