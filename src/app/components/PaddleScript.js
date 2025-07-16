2910532d0e4504de016226b00698a04bf921a900
YoussufSalah <youssufsalahsaber@gmail.com>
2025-07-16 14:56:22 +0300

    Fixed PaddleScript

--- src/app/components/PaddleScript.js
+++ src/app/components/PaddleScript.js
@@ -1,15 +1,22 @@
-<Script
-    src="https://cdn.paddle.com/paddle/v2/paddle.js"
-    strategy="beforeInteractive"
-    onLoad={() => {
-        if (typeof window !== "undefined" && window.Paddle) {
-            window.Paddle.Environment.set("sandbox");
-            window.Paddle.Initialize({
-                token: process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN,
-                eventCallback: function (data) {
-                    console.log(data);
-                },
-            });
-        }
-    }}
-/>;
+"use client";
+
+import Script from "next/script";
+export default function PaddleScript() {
+    return (
+        <Script
+            src="https://cdn.paddle.com/paddle/v2/paddle.js"
+            strategy="beforeInteractive"
+            onLoad={() => {
+                if (typeof window !== "undefined" && window.Paddle) {
+                    window.Paddle.Environment.set("sandbox");
+                    window.Paddle.Initialize({
+                        token: process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN,
+                        eventCallback: function (data) {
+                            console.log(data);
+                        },
+                    });
+                }
+            }}
+        />
+    );
+}
