97f44b282949c6f953b84583326f25e09161dc7f
YoussufSalah <youssufsalahsaber@gmail.com>
2025-07-16 14:34:28 +0300

    Updated plans page sample button

--- src/app/plans/page.js
+++ src/app/plans/page.js
@@ -1,6 +1,8 @@
 "use client";
 
-const products = {
+import { apiService } from "@/utils/APIService";
+
+const PRODUCTS = {
     starter_monthly: "pri_01k05grsa4vqw35evbbj16scvb",
     starter_annually: "pri_01k05gx1bbtpna0kjzh92jvmf2",
     pro_monthly: "pri_01k05gyeyf1wqt7we25jygtre7",
@@ -8,25 +10,32 @@ const products = {
 };
 
 export default function PlansPage() {
-    function openCheckout(items, customer) {
-        window.Paddle.Checkout.open({
-            items: items,
-            customer: customer,
-        });
-    }
     return (
         <div className="flex flex-row items-center justify-around p-4 m-4">
             <div className="flex flex-col gap-2">
                 <h3>
                     <code>Starter Monthly ($3.99)</code>
                 </h3>
-                <a
-                    href="#"
-                    className="bg-emerald-700 text-emerald-100 w-40 h-20 rounded-xl"
-                    onClick={openCheckout(products.starter_monthly)}
+                <button
+                    className="bg-emerald-800 text-emerald-100 w-30 h-15 rounded-[8px]"
+                    onClick={() => {
+                        window.Paddle.Checkout.open({
+                            items: [
+                                {
+                                    priceId: PRODUCTS.starter_monthly,
+                                    quantity: 1,
+                                },
+                            ],
+                            customer: {
+                                token: apiService.isAuthenticated()
+                                    ? apiService.getToken()
+                                    : "unauthinticated",
+                            },
+                        });
+                    }}
                 >
-                    <b>Sign up now</b>
-                </a>
+                    Subscribe – Starter Monthly ($3.99)
+                </button>
             </div>
         </div>
     );
