ff6c303ef8fe489fab2ed6c431dbd08dd5da23f4
Prince Chidera <princechidera1007@gmail.com>
2025-07-26 14:28:33 +0200

    back to last good commit

--- src/app/upload/page.js
+++ src/app/upload/page.js
@@ -703,7 +703,7 @@ const PDFUploadPage = () => {
                                             onClick={() => downloadMarkdownPDF(results.summary.content, "PrepPal-Summary.pdf")}
                                             className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-all"
                                             >
-                                                Download as PDF
+                                                Download as PDF.
                                             </button>
                                         </div>
                                     )}
